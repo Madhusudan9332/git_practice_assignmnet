704dd8128b73d1521eceecb0188423216b3acf5a
DkBhardwaj52 <dheerajbhardwaj77212gmail.com>
2022-11-03 21:41:55 +0530

    prime_update

--- prime.js
+++ prime.js
@@ -1,3 +1,15 @@
 function prime(num){
-	return true;
-}
+	let count=0;
+	 for(let x=1 ; x<=num ; x++ ){
+		if(num%x==0){
+			count++
+		}
+	 }
+	
+	if(count==2){
+		return  ture;
+	}else{
+		return false;
+	}
+	
+	 }console.log(prime(18));
